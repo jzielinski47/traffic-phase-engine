b1154d799f303d4792df9cdcb42f2cfc3387a561
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-05-23 16:42:15 +0200

    implementation of hash map

--- src/main/java/com/jzielinski/controller/SimulationController.java
+++ src/main/java/com/jzielinski/controller/SimulationController.java
@@ -6,31 +6,30 @@ import com.jzielinski.domain.model.Vehicle;
 import com.jzielinski.enums.Direction;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SimulationController {
 
-    protected Road northRoad = new Road(Direction.north) {
-    };
-    protected Road eastRoad = new Road(Direction.east) {
-    };
-    protected Road southRoad = new Road(Direction.south) {
-    };
-    protected Road westRoad = new Road(Direction.west) {
-    };
 
+    Map<Direction, Road> roads = new HashMap<>();
     ArrayList<Command> commands;
 
     public SimulationController(ArrayList<Command> commands) {
         this.commands = commands;
+        roads.put(Direction.north, new Road(Direction.north));
+        roads.put(Direction.south, new Road(Direction.south));
+        roads.put(Direction.east, new Road(Direction.east));
+        roads.put(Direction.west, new Road(Direction.west));
     }
 
     public void runSimulation() {
         commands.forEach(command -> {
             switch (command.getType()) {
                 case addVehicle:
-                    Road road = resolveRoad(command.getStartRoad());
                     Vehicle vehicle = new Vehicle(command.getVehicleId(), command.getStartRoad(), command.getEndRoad());
+                    Road road = roads.get(command.getStartRoad());
                     Objects.requireNonNull(road).addVehicle(vehicle);
                     System.out.println("process addVehicle " + vehicle.getId());
                     break;
@@ -42,20 +41,7 @@ public class SimulationController {
         });
     }
 
-    private Road resolveRoad(Direction dir) {
-        switch (dir) {
-            case north:
-                return northRoad;
-            case east:
-                return eastRoad;
-            case south:
-                return southRoad;
-            case west:
-                return westRoad;
-            default:
-                return null;
-        }
-    }
+
 
 
 }
